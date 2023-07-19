a824c1a2bbc99691a7ca67bd3af6fe9e1e221008
Ingsam3 <leeye1021@naver.com>
2023-07-19 13:23:19 +0900

    spring 게시판 예제 start

--- spring_mvc/src/main/webapp/sql/tbl_member.sql
+++ spring_mvc/src/main/webapp/sql/tbl_member.sql
@@ -8,3 +8,25 @@ userid varchar2(50) primary key --회원 아이디
 );
 
 select *from tbl_member;
+
+
+create table tbl_board(
+bno number(38) primary key --게시판 번호
+,writer varchar2(100) not null --작성자
+,title varchar2(200) not null --제목
+,content varchar2(4000) --내용
+,viewcnt number(38) default 0 
+-- default 0 조회수 제약조
+-- insert 건을 주면 해당컬럼에 레코드를 하지 않으면 기본값
+--0 . 이 저장됨
+,regdate date DEFAULT sysdate--등록날짜
+);
+select * from tbl_board order by bno desc;
+
+--시퀀스 생성
+create sequence bno_seq
+increment by 1 --1씩 증가 옵션
+start with 1 -- 1부터 시작
+nocache;
+--시퀀스 번호값 발생
+select bno_seq.nextval from dual;
