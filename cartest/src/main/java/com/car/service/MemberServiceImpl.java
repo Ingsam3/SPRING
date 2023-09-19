60255edc82b0cb4ce9bb057df101edf31a7ccbfa
Ingsam3 <leeye1021@naver.com>
2023-09-19 15:11:24 +0900

    비밀번호 찾기 jsp 부분 추가

--- cartest/src/main/java/com/car/service/MemberServiceImpl.java
+++ cartest/src/main/java/com/car/service/MemberServiceImpl.java
@@ -54,60 +54,7 @@ public class MemberServiceImpl implements MemberService {
 		
 		return this.memberDao.serchUserEmail(m_email);
 	}
-	/*
-	@Override
-	public int sendMail(String m_email) {
-		String test = "leeye1021@naver.com";
-		int number =0;
-        if(m_email.equals(test.trim())) {
-        	
-        	number =1; 
-        	
-        }else {
-        	number =0000;
-        }
-        return number;
-		
-		
-	}//이메일 보내기
 	
-	 public int sendMail(String m_email){
-	    	
-	        MimeMessage message = CreateMail(m_email);
-	        javaMailSender.send(message);
-
-	        return number;
-	    }
-		
-	
-	
-	 private JavaMailSender javaMailSender;
-	    private static final String senderEmail= "dldpdnjs1021@gmail.com";
-	    private static int number;
-	
-	 public static void createNumber(){
-	        number = (int)(Math.random() * (90000)) + 100000;// (int) Math.random() * (최댓값-최소값+1) + 최소값
-	    }//랜덤 번호 생성
-	 
-	 public MimeMessage CreateMail(String m_email){
-	        createNumber();
-	        MimeMessage message = javaMailSender.createMimeMessage();
-
-	        try {
-	            message.setFrom(senderEmail);
-	            message.setRecipients(MimeMessage.RecipientType.TO, m_email);
-	            message.setSubject("이메일 인증");
-	            String body = "";
-	            body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
-	            body += "<h1>" + number + "</h1>";
-	            body += "<h3>" + "감사합니다." + "</h3>";
-	            message.setText(body,"UTF-8", "html");
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
-
-	        return message;
-	    }*/
 
 
 	}
